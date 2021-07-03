49228bac5c391b3f631b270ceb7a780aaacd7be3
최주혜 <zue@kakao.com>
2021-07-03 10:01:27 +0900

    Initial commit

---
+++ src/main/java/com/zue/java/zuefirstjava/ZueFirstJavaApplication.java
@@ -0,0 +1,13 @@
+package com.zue.java.zuefirstjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZueFirstJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZueFirstJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zue/java/zuefirstjava/ZueFirstJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zue.java.zuefirstjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZueFirstJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
